6564b1e98e4c37d273f63e0c257dc776ef8b7410
Pranay Garg <36327069+pranay2000garg@users.noreply.github.com>
2022-03-02 13:21:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+app.post('/bfhl',(req,res)=>{
+    let status = true;
+    try{
+    let num_Arr = []
+    let alf_Arr = []
+    let arr = req.body.data
+    for(let i=0;i<arr.length;i++){
+        if(!isNaN(arr[i])){
+            num_Arr.push(arr[i])
+        }else{
+            alf_Arr.push(arr[i])
+        }
+    }
+    let ans = {
+        is_success: status,
+        user_id: "pranay_garg_26102000",
+        email: "pranaygarg.cse19@chitkarauniversity.edu.in",
+        roll_number: "1911981078",
+        numbers:num_Arr,
+        alphabets:alf_Arr
+    }
+    res.send(ans)
+    }
+    catch(err){
+        status = false;
+        res.send({
+            is_success: status
+        })
+    }
+})
+
+app.listen(8000, () => {
+    console.log("Server is running at port 8000");
+});
